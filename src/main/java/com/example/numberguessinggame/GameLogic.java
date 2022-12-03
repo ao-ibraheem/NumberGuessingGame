0eee210adbdacbbf84d59ba6f454c98a32590a23
aoibraheem <aoibraheem@gmail.com>
2022-12-03 17:20:47 +0000

    Game logic fixed and added game reset

--- src/main/java/com/example/numberguessinggame/GameLogic.java
+++ src/main/java/com/example/numberguessinggame/GameLogic.java
@@ -11,7 +11,7 @@ public class GameLogic {
 
     public void setRandomNumber() {
         Random random = new Random();
-        randomNumber = random.nextInt(10);
+        randomNumber = random.nextInt(50);
         System.out.println(randomNumber);
     }
      public boolean isNum(int guess){
@@ -21,13 +21,16 @@ public class GameLogic {
 
      public void win(int guess){
         if(isNum(guess)){
+            System.out.println("You win");
+            System.out.println("Game will reset");
+            setRandomNumber();
             Popup popup = new Popup();
             Label label = new Label("You've guessed it right!");
             label.setStyle("-fx-background-color: pink;");
             popup.getContent().add(label);
             label.setMinWidth(80);
             label.setMinHeight(50);
-            i
+
         }
      }
 
--- src/main/java/com/example/numberguessinggame/GuessingController.java
+++ src/main/java/com/example/numberguessinggame/GuessingController.java
@@ -3,16 +3,27 @@ package com.example.numberguessinggame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class GuessingController {
+    GameLogic logic = new GameLogic();
     @FXML
     private TextField tf;
 
     @FXML
+    private VBox vb;
+
+    @FXML
+    private void setVBox(){
+        vb.setStyle("-fx-background-color: black");
+    }
+
+
+    @FXML
     protected void onClick() {
+        vb.setStyle("-fx-background-color: black");
         String numStr = tf.getText();
         int num = Integer.parseInt(numStr);
-        GameLogic logic = new GameLogic();
         logic.win(num);
 
     }
